eaee9e1f47b50265875e3ab5dc76921ef7829bcc
lou <lashlou@rambler.ru>
2022-03-27 13:52:56 +0300

    Extend solve for two roots

--- solve.h
+++ solve.h
@@ -8,7 +8,12 @@ bool fuzzy_eq (double a, double b, double min_compare = 10e-12)
   return fabs (a - b) < min_compare;
 }
 
-std::vector<double> solve (double, double, double)
+std::vector<double> solve (double a, double b, double c)
 {
+  double D = b * b - 4 * a * c;
+
+  if (D > 0)
+    return {(- b - sqrt (D)) / (2 * a), (- b + sqrt (D)) / (2 * a)};
+
   return {};
 }
