5a5f352889dbfa81c48bbc3b722f70069fa4c244
lou <lashlou@rambler.ru>
2022-03-27 14:21:17 +0300

    Expand tests for fuzzy equality discriminant to 0 case

--- tests.h
+++ tests.h
@@ -26,9 +26,14 @@ TEST (hw1_tests, two_roots_test)
 
 TEST (hw1_tests, one_root_test)
 {
-  std::vector<double> roots = solve (1., 2., 1.);
-  EXPECT_TRUE (roots.size () == 1);
-  EXPECT_TRUE (fuzzy_eq (roots[0], -1));
+  auto one_root_value_test = [] (double a, double b, double c, double root)
+  {
+    std::vector<double> roots = solve (a, b, c);
+    EXPECT_TRUE (roots.size () == 1);
+    EXPECT_TRUE (fuzzy_eq (roots[0], root));
+  };
+  one_root_value_test (1., 2., 1., -1.);
+  one_root_value_test (1., 2., 1. + 10e-14, -1.);
 }
 
 TEST (hw1_tests, zero_exception_test)
